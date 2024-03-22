8702fcba4c1c768b089acb1b0f7fba926fd197e3
amitathik7 <amitathik7@gmail.com>
2024-03-22 19:28:25 -0400

    Changed the from a adjacency matrix to an adjacency list.

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -6,13 +6,24 @@
 
 using namespace std;
 
+struct Node {
+    Node* next;
+    int to_index;
+    int outdegree;
+    Node(int to_index, int out_degree) {
+        this->next = nullptr;
+        this->to_index = to_index;
+        this->outdegree = out_degree;
+    }
+};
+
 class AdjacencyList {
-    private:
-    vector<vector<int>> matrix;
+private:
+    vector<Node*> matrix;
     unordered_map<string, int> mp;
     vector<int> page_rank;
     int num_websites;
-    public:
+public:
     AdjacencyList();
     void add_edge(string from, string to);
     void power_iteration();
