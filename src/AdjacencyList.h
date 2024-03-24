066e2904494088d5b1f8857800f934e8fcf54204
amitathik7 <amitathik7@gmail.com>
2024-03-24 16:48:30 -0400

    Add the function declarations and variables in AdjacencyList.h

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,7 +1,18 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 class AdjacencyList {
-
+private:
+    std::unordered_map<std::string, std::string> mp;
+    std::vector<std::vector<int>> matrix;
+    std::vector<int> out_degree;
+    std::vector<double> page_rank;
+    int num_websites;
+public:
+    void multiply();
+    std::string get_page_rank();
+    void add_edge(std::string from, std::string to);
 };
