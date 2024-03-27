79b007fc9a47e78c08a189b117b551a05ac7819b
amitathik7 <amitathik7@gmail.com>
2024-03-27 15:50:03 -0400

    Fixed main.cpp main() function.

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,25 @@
 using namespace std;
 
 int main() {
-    string input = R"(7 2
-google.com gmail.com
-google.com maps.com
-facebook.com ufl.edu
-ufl.edu google.com
-ufl.edu gmail.com
-maps.com facebook.com
-gmail.com maps.com)";
+    int no_of_lines, power_iterations;
 
-    AdjacencyList temp;
+    string from, to;
 
-    temp.read_whole_input(input);
+    cin >> no_of_lines;
+    cin >> power_iterations;
 
-//    int no_of_lines, power_iterations;
-//
-//    string from, to;
-//
-//    cin >> no_of_lines;
-//    cin >> power_iterations;
-//
-//    AdjacencyList list;
-//
-//    for (int i = 0; i < no_of_lines; i++) {
-//        cin >> from;
-//        cin >> to;
-//
-//        list.add_edge(from, to);
-//    }
-//
-//    for (int i = 0; i < power_iterations; i++) {
-//        list.multiply();
-//    }
-//
-//    list.print_page_rank();
+    AdjacencyList list;
+
+    for (int i = 0; i < no_of_lines; i++) {
+        cin >> from;
+        cin >> to;
+
+        list.add_edge(from, to);
+    }
+
+    for (int i = 0; i < power_iterations; i++) {
+        list.multiply();
+    }
+
+    list.print_page_rank();
 }
